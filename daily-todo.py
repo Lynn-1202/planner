0414c5fafc43b7229590934433bb6c56149e61de
Lynn-1202 <ldn1202@163.com>
2026-07-01 15:48:05 +0800

    add error tracing

--- daily-todo.py
+++ daily-todo.py
@@ -217,4 +217,9 @@ def main():
             json={"receive_id": OPEN_ID, "msg_type": "text", "content": content}, timeout=15)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        sys.exit(1)
